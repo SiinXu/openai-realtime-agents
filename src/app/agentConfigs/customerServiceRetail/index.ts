f6738e630a12e096886e3c58d329aaedf8327cf8
Siin Xu <31815270+SiinXu@users.noreply.github.com>
2025-06-30 21:32:58 +0800

    Update index.ts

--- src/app/agentConfigs/customerServiceRetail/index.ts
+++ src/app/agentConfigs/customerServiceRetail/index.ts
@@ -2,20 +2,23 @@ import { authenticationAgent } from './authentication';
 import { returnsAgent } from './returns';
 import { salesAgent } from './sales';
 import { simulatedHumanAgent } from './simulatedHuman';
+import { cindyAgent } from './cindy';
 
 // Cast to `any` to satisfy TypeScript until the core types make RealtimeAgent
 // assignable to `Agent<unknown>` (current library versions are invariant on
 // the context type).
-(authenticationAgent.handoffs as any).push(returnsAgent, salesAgent, simulatedHumanAgent);
-(returnsAgent.handoffs as any).push(authenticationAgent, salesAgent, simulatedHumanAgent);
-(salesAgent.handoffs as any).push(authenticationAgent, returnsAgent, simulatedHumanAgent);
-(simulatedHumanAgent.handoffs as any).push(authenticationAgent, returnsAgent, salesAgent);
+(authenticationAgent.handoffs as any).push(returnsAgent, salesAgent, simulatedHumanAgent, cindyAgent);
+(returnsAgent.handoffs as any).push(authenticationAgent, salesAgent, simulatedHumanAgent, cindyAgent);
+(salesAgent.handoffs as any).push(authenticationAgent, returnsAgent, simulatedHumanAgent, cindyAgent);
+(simulatedHumanAgent.handoffs as any).push(authenticationAgent, returnsAgent, salesAgent, cindyAgent);
+(cindyAgent.handoffs as any).push(authenticationAgent, returnsAgent, salesAgent, simulatedHumanAgent);
 
 export const customerServiceRetailScenario = [
   authenticationAgent,
   returnsAgent,
   salesAgent,
   simulatedHumanAgent,
+  cindyAgent,
 ];
 
 // Name of the company represented by this agent set. Used by guardrails
